ce6cec11e68eabeaa536020de3c2c112c992d08e
chinh trinh <chinhtrinh@cqtdt.asia>
2022-12-30 13:21:23 +0700

    develop UI list question

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,1 +1,25 @@
 export const LOGO_IMAGE_URL = "https://stackoverflow.design/assets/img/logos/so/logo-stackoverflow.png"
+
+export const LIST_QUESTIONS = [
+    {
+        title: "Entity Framework: How to get desired json response?",
+        votesNumber: 3,
+        answersNumber: 4,
+        viewsNumber: 8,
+        createAt: '20/1/2023'
+    },
+    {
+        title: "SwiftUI bug when displaying multiple line value on left side and which breaks alignment on right side in stack",
+        votesNumber: 13,
+        answersNumber: 1,
+        viewsNumber: 82,
+        createAt: '20/1/2023'
+    },
+    {
+        title: "JavaScript regex to match the very precise number of the same characters",
+        votesNumber: 0,
+        answersNumber: 0,
+        viewsNumber: 1,
+        createAt: '19/1/2023'
+    }
+]
