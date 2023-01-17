182f61bacb3ae99a5b9637d549926254ee02a915
Duong Huong <huongduong@cqtdt.asia>
2023-01-17 17:44:10 +0700

    fix comment

--- src/api/api.setting.ts
+++ src/api/api.setting.ts
@@ -4,7 +4,6 @@ export class ApiSettings {
   public static API_POST_REGISTER = "api/auth/register";
   public static API_POST_LOGIN = "api/auth/login";
   public static API_GET_VOTE_DETAIL = "api/vote/question/";
-  public static API_GET_VOTE_TYPE = "api/vote/status?questionId=";
   public static API_VOTE = "api/vote";
   public static API_GET_ALL_USER = "api/user";
   public static API_REGISTER_VERITY = "api/auth/verify";
